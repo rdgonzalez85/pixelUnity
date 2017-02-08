fdd4e18328173545b8d9e9913e7fbb50f89080bc
rdgonzalez <rdgonzalez85@gmail.com>
2017-02-08 15:11:23 -0500

    created MoveCamera.cs updated Movement.cs created new scene

--- Assets/scripts/Movement.cs
+++ Assets/scripts/Movement.cs
@@ -1,30 +1,108 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movement : MonoBehaviour {
-	public float moveSpeed = 0.0f;
-	private Rigidbody rigidbody;
-
-	// Use this for initialization
-	void Start() 
-	{
-		rigidbody = GetComponent<Rigidbody>();
-	}
+public class Movement : MonoBehaviour
+{
 	
-	// Update is called once per frame
-	void Update () {
-		transform.Translate(new Vector3(Input.GetAxis("Vertical"),
-			0,
-			- Input.GetAxis("Horizontal")) * moveSpeed);
+	public float speed = 6f;
+	// The speed that the player will move at.
+
+	Vector3 movement;
+	// The vector to store the direction of the player's movement.
+	Rigidbody playerRigidbody;
+	// Reference to the player's rigidbody.
+	int floorMask;
+	// A layer mask so that a ray can be cast just at gameobjects on the floor layer.
+	float camRayLength = 500f;
+	// The length of the ray from the camera into the scene.
+
+	public float jumpSpeed = 4.0f;
+	private bool hasJumped = false;
+	private bool grounded = true;
+
+	void Awake ()
+	{
+		// Create a layer mask for the floor layer.
+		floorMask = LayerMask.GetMask ("Floor");
+		// Set up references.
+		playerRigidbody = GetComponent <Rigidbody> ();
 	}
 
-	void OnTriggerEnter(Collider other) {
-		//Destroy(other.gameObject);
+
+	void FixedUpdate ()
+	{
+
+		// Store the input axes.
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+		// Move the player around the scene.
+		Move (h, v);
+
+		// Turn the player to face the mouse cursor.
+		Turning ();
+
+		if (hasJumped && grounded) {
+			hasJumped = false;
+			grounded = false;
+			Jump ();
+		}
+
 	}
 
-	void FixedUpdate () {
-	//	rigidbody.AddForce(new Vector3(Input.GetAxis("Vertical"),
-	//		0,
-	//		- Input.GetAxis("Horizontal")) * moveSpeed);
+	void Update() {
+		if (!grounded && playerRigidbody.velocity.y == 0.0f) {
+			grounded = true;
+		}
+
+		if (Input.GetButton ("Jump")) {
+			hasJumped = true;
+		}
 	}
+
+	void Jump ()
+	{
+		movement.Set (movement.x, jumpSpeed, movement.z);
+
+		// Normalise the movement vector and make it proportional to the speed per second.
+		//movement = movement.normalized * jumpSpeed * Time.deltaTime;
+
+		// Move the player to it's current position plus the movement.
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+
+	void Move (float h, float v)
+	{
+		// Set the movement vector based on the axis input.
+		movement.Set (h, 0f, v);
+
+		// Normalise the movement vector and make it proportional to the speed per second.
+		movement = movement.normalized * speed * Time.deltaTime;
+
+		// Move the player to it's current position plus the movement.
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+
+	void Turning ()
+	{
+		// Create a ray from the mouse cursor on screen in the direction of the camera.
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		// Create a RaycastHit variable to store information about what was hit by the ray.
+		RaycastHit floorHit;
+
+		// Perform the raycast and if it hits something on the floor layer...
+		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) {
+			// Create a vector from the player to the point on the floor the raycast from the mouse hit.
+			Vector3 playerToMouse = floorHit.point - transform.position;
+
+			// Ensure the vector is entirely along the floor plane.
+			playerToMouse.y = 0f;
+
+			// Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+
+			// Set the player's rotation to this new rotation.
+			playerRigidbody.MoveRotation (newRotation);
+		}
+	}
+		
 }
