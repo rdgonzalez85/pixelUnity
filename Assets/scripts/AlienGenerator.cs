2335f0541f2d412b5bbbf26888dba1adeb177548
Rodrigo Gonzalez <rdgonzalez85@gmail.com>
2017-02-07 22:17:18 -0500

    initial commit

---
+++ Assets/scripts/AlienGenerator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienGenerator : MonoBehaviour {
+
+	public GameObject[] aliens;
+	public float spawnRate = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (SpawnAlien ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	IEnumerator SpawnAlien() {
+		yield return new WaitForSeconds (2.0f);
+		while (true) {
+			//Random r = new Random();
+			int rInt = Random.Range(0,aliens.Length);
+			Vector3 position = new Vector3 (0.0f, transform.position.y, transform.position.z);
+
+			Instantiate (aliens[rInt], position, Quaternion.identity);
+			yield return new WaitForSeconds(spawnRate);
+		}
+	}
+}
---
+++ Assets/scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public float moveSpeed = 0.0f;
+	private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start() 
+	{
+		rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector3(Input.GetAxis("Vertical"),
+			0,
+			- Input.GetAxis("Horizontal")) * moveSpeed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		//Destroy(other.gameObject);
+	}
+
+	void FixedUpdate () {
+	//	rigidbody.AddForce(new Vector3(Input.GetAxis("Vertical"),
+	//		0,
+	//		- Input.GetAxis("Horizontal")) * moveSpeed);
+	}
+}
